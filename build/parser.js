fdea9d552c4ae266e62a18040fec8e9c5dc012ce
Harry Merzin <hmerzin@gmail.com>
2017-03-18 20:05:13 -0400

    make happy the linter

--- build/parser.js
+++ build/parser.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 exports.default = parse;
 
@@ -14,10 +14,9 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function parse(fileName, callback) {
-    var tokenized = [];
-    _fs2.default.readFile(fileName, 'utf-8', function (err, script) {
-        callback(tokenize(script), script);
-    });
+  _fs2.default.readFile(fileName, 'utf-8', function (err, script) {
+    callback(tokenize(script), script);
+  });
 }
 
 /*
@@ -28,79 +27,81 @@ parse('test.hl', (ast, script) => {
 */
 
 var Declaration = function Declaration(name, value) {
-    _classCallCheck(this, Declaration);
+  _classCallCheck(this, Declaration);
 
-    this.name = name;
-    this.value = value;
+  this.name = name;
+  this.value = value;
 };
 
 var CallFunc = function CallFunc(funcName) {
-    _classCallCheck(this, CallFunc);
+  _classCallCheck(this, CallFunc);
 
-    this.funcName = funcName;
+  this.funcName = funcName;
 };
 
-var FunctionLine = function FunctionLine(type, value) {
-    _classCallCheck(this, FunctionLine);
-
+/*
+class FunctionLine {
+  constructor(type, value) {
     this.type = type;
     this.value = value;
-};
+  }
+}
+*/
 
 var Function = function Function(name) {
-    _classCallCheck(this, Function);
+  _classCallCheck(this, Function);
 
-    this.name = name;
-    //this.lines = lines;
-    //this.value = value;
+  this.name = name;
+  // this.lines = lines;
+  // this.value = value;
 };
 
 function tokenize(script) {
-    var ast = [];
-    var lines = script.split('\n');
-    lines.forEach(function (line, index) {
-        if (line.split(' ')[0] == 'let') {
-            ast.push(declare(line));
-        } else if (line.split(' ')[0] == 'func') {
-            var funcLines = [];
-            var counter = index++;
-            var name = line.split(' ')[1].split('()')[0];
-            while (!lines[counter].includes('}')) {
-                funcLines.push(lines[counter]);
-                counter++;
-            }
-            ast.push(declareFunc(name, funcLines));
-        }
-    });
-    //console.log(ast);
-    return ast;
+  var ast = [];
+  var lines = script.split('\n');
+  lines.forEach(function (line, index) {
+    if (line.split(' ')[0] == 'let') {
+      ast.push(declare(line));
+    } else if (line.split(' ')[0] === 'func') {
+      var funcLines = [];
+      var counter = index + 1;
+      var name = line.split(' ')[1].split('()')[0];
+      while (!lines[counter].includes('}')) {
+        funcLines.push(lines[counter]);
+        counter += 1;
+      }
+      ast.push(declareFunc(name, funcLines));
+    }
+  });
+  // console.log(ast);
+  return ast;
 }
 
 function declare(line) {
-    var dec = line.split(' ');
-    var name = dec[1];
-    var value = dec[3].split('"')[1];
-    return new Declaration(name, value);
+  var dec = line.split(' ');
+  var name = dec[1];
+  var value = dec[3].split('"')[1];
+  return new Declaration(name, value);
 }
 
 function callFunction(name) {
-    return new CallFunc(name);
+  return new CallFunc(name);
 }
 
 function declareFunc(name, lines) {
-    var func = new Function(name);
-    var parsedLines = [];
-    lines.forEach(function (line) {
-        if (line.split(' ')[4] == 'let') {
-            parsedLines.push(declare(line));
-        } else if (line.split(' ')[0].includes('()')) {
-            parsedLines.push(callFunction(line.split('()')[0]));
-        } else if (line.split(' ')[0].includes('return')) {
-            func.value = line.split(' ')[1];
-        }
-    });
-    //console.log(lines);
-    func.lines = parsedLines;
-    func.value = null;
-    return func;
+  var func = new Function(name);
+  var parsedLines = [];
+  lines.forEach(function (line) {
+    if (line.split(' ')[4] === 'let') {
+      parsedLines.push(declare(line));
+    } else if (line.split(' ')[0].includes('()')) {
+      parsedLines.push(callFunction(line.split('()')[0]));
+    } else if (line.split(' ')[0].includes('return')) {
+      func.value = line.split(' ')[1];
+    }
+  });
+  // console.log(lines);
+  func.lines = parsedLines;
+  func.value = null;
+  return func;
 }
--- src/parser.js
+++ src/parser.js
@@ -1,10 +1,9 @@
 import fs from 'fs';
 
 export default function parse(fileName, callback) {
-    let tokenized = [];
-    fs.readFile(fileName, 'utf-8', (err, script) => {
-        callback(tokenize(script), script);
-    });
+  fs.readFile(fileName, 'utf-8', (err, script) => {
+    callback(tokenize(script), script);
+  });
 }
 
 /*
@@ -15,79 +14,81 @@ parse('test.hl', (ast, script) => {
 */
 
 class Declaration {
-    constructor(name, value) {
-        this.name = name;
-        this.value = value;
-    }
+  constructor(name, value) {
+    this.name = name;
+    this.value = value;
+  }
 }
 
 class CallFunc {
-    constructor(funcName) {
-        this.funcName = funcName;
-    }
+  constructor(funcName) {
+    this.funcName = funcName;
+  }
 }
 
+/*
 class FunctionLine {
-    constructor(type, value) {
-        this.type = type;
-        this.value = value;
-    }
+  constructor(type, value) {
+    this.type = type;
+    this.value = value;
+  }
 }
+*/
 
 class Function {
-    constructor(name) {
-        this.name = name;
-        //this.lines = lines;
-        //this.value = value;
-    }
+  constructor(name) {
+    this.name = name;
+    // this.lines = lines;
+    // this.value = value;
+  }
 }
 
 function tokenize(script) {
-    let ast = [];
-    const lines = script.split('\n');
-    lines.forEach((line, index) => {
-        if(line.split(' ')[0] == 'let') {
-            ast.push(declare(line));
-        } else if(line.split(' ')[0] == 'func') {
-            let funcLines = [];
-            let counter = index ++;
-            let name = line.split(' ')[1].split('()')[0];
-            while(!lines[counter].includes('}')) {
-                funcLines.push(lines[counter]);
-                counter ++;
-            }
-            ast.push(declareFunc(name, funcLines));
-        }
-    });
-    //console.log(ast);
-    return ast;
+  const ast = [];
+  const lines = script.split('\n');
+  lines.forEach((line, index) => {
+    if (line.split(' ')[0] == 'let') {
+      ast.push(declare(line));
+    } else if (line.split(' ')[0] === 'func') {
+      const funcLines = [];
+      let counter = index + 1;
+      const name = line.split(' ')[1].split('()')[0];
+      while (!lines[counter].includes('}')) {
+        funcLines.push(lines[counter]);
+        counter += 1;
+      }
+      ast.push(declareFunc(name, funcLines));
+    }
+  });
+  // console.log(ast);
+  return ast;
 }
 
 function declare(line) {
-    const dec = line.split(' ');
-    const name = dec[1];
-    const value = dec[3].split('"')[1];
-    return (new Declaration(name, value));
+  const dec = line.split(' ');
+  const name = dec[1];
+  const value = dec[3].split('"')[1];
+  return (new Declaration(name, value));
 }
 
 function callFunction(name) {
-    return new CallFunc(name);
+  return new CallFunc(name);
 }
 
 function declareFunc(name, lines) {
-    let func = new Function(name);
-    let parsedLines = [];
-    lines.forEach(line => {
-        if(line.split(' ')[4] == 'let') {
-            parsedLines.push(declare(line));
-        } else if (line.split(' ')[0].includes('()')) {
-            parsedLines.push(callFunction(line.split('()')[0]));
-        } else if(line.split(' ')[0].includes('return')) {
-            func.value = line.split(' ')[1];
-        }
-    });
-    //console.log(lines);
-    func.lines = parsedLines;
-    func.value = null;
-    return func;
+  const func = new Function(name);
+  const parsedLines = [];
+  lines.forEach((line) => {
+    if (line.split(' ')[4] === 'let') {
+      parsedLines.push(declare(line));
+    } else if (line.split(' ')[0].includes('()')) {
+      parsedLines.push(callFunction(line.split('()')[0]));
+    } else if (line.split(' ')[0].includes('return')) {
+      func.value = line.split(' ')[1];
+    }
+  });
+  // console.log(lines);
+  func.lines = parsedLines;
+  func.value = null;
+  return func;
 }
